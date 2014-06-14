f91800754ccdf78e7a12462eb07c01fb0860b234
Vyacheslav Levit <levit.slava@gmail.com>
2014-06-14 18:15:48 +0300

    Add facebook email to connection values

--- flask_social/providers/facebook.py
+++ flask_social/providers/facebook.py
@@ -56,7 +56,8 @@ def get_connection_values(response, **kwargs):
         access_token=access_token,
         secret=None,
         display_name=profile.get('username', None),
-        full_name = profile.get('name', None),
+        full_name=profile.get('name', None),
         profile_url=profile_url,
-        image_url=image_url
+        image_url=image_url,
+        email=profile.get('email', None)
     )
